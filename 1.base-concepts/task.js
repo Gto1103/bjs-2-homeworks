53a092e7e6f36a956797225f6cd1f7e2974e1e6b
Vladislav Pogorelyi <gto1103@gmail.com>
2021-08-09 22:07:42 +0300

    Change app.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,23 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+	let arr;
+	let D = b ** - 4 * a * c;
+
+	if (D < 0) {
+
+	}
+
+	// код для задачи №1 писать здесь
+	return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+	let totalAmount;
 
-  // код для задачи №2 писать здесь
 
-  return totalAmount;
+
+	// код для задачи №2 писать здесь
+
+	return totalAmount;
 }
