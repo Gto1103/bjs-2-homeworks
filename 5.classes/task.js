dabeee2296ca313f149511f6c48f5e4358c7f3a7
Vladislav Pogorelyi <gto1103@gmail.com>
2021-09-09 23:59:59 +0300

    5 task

---
+++ 5.classes/task.js
@@ -0,0 +1,100 @@
+//Задача 1
+
+class PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.pagesCount = pagesCount;
+		this.state = 100;
+		this.type = null;
+	}
+
+	fix() {
+		this.state = this.state * 1.5;
+	}
+
+	set state(newState) {
+		if (newState < 0) {
+			this.state = 0;
+		} else if (newState > 100) {
+			this.state = 100
+		} else {
+			this._state = newState;
+		}
+	}
+
+	get state() {
+		return this._state;
+	}
+}
+
+class Magazine extends PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = "magazine";
+	}
+}
+
+class Book extends PrintEditionItem {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.author = author;
+		this.type = "book";
+	}
+}
+
+class NovelBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = "novel";
+	}
+}
+
+class FantasticBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = "fantastic";
+	}
+}
+
+class DetectiveBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = "detective";
+	}
+}
+
+//Задача 2. Библиотека
+
+class Library {
+	constructor(name, books) {
+		this.name = name;
+		this.books = [];
+	}
+
+	addBook(book) {
+		if (book.state > 30) {
+			this.books.push(book);
+		};
+	}
+
+	findBookBy(type, value) {
+
+		let findBookIndex = this.books.findIndex((item, idx) => item[type] === value);
+
+		if (findBookIndex != -1) {
+			return this.books[findBookIndex];
+		} else return null;
+	}
+
+	giveBookByName(bookName) {
+
+		let findBookByName = this.books.findIndex((item, idx) => item.name === bookName);
+
+		if (findBookByName != -1) {
+			const deleteBook = this.books[findBookByName];
+			this.books.splice([findBookByName], 1)
+			return deleteBook;
+		} else return null;
+	}
+}
